8e6af06ba67e7fb1f1207bad7b1499edb3d95731
Marco Cilona <marco.cilona@gmail.com>
2018-07-13 14:24:47 +0200

    Added cmkEditor.

---
+++ PHPforBeginners/cms/admin/includes/templates/footer.php
@@ -0,0 +1,18 @@
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    
+    <!-- including google chart -->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src ="js/chart.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	
+	<!-- ckeditor -->
+	<script src="../cmk/ckeditor/ckeditor.js"></script>
+    <script src="js/ckeditor.js"></script>
+
+</body>
+
+</html>
