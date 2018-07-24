c39397ff4864ebb5ae385bebc6b2bcce9f3d8d27
Marco Cilona <marco.cilona@gmail.com>
2018-07-24 22:53:41 +0200

    Laravel Raw SQL Queries lectures (CRUD operations).

--- Laravel/cms/app/Http/routes.php
+++ Laravel/cms/app/Http/routes.php
@@ -24,3 +24,31 @@ Route::get('/contact/{name?}', 'PostController@show_view');
  * Using the resource function to get Laravel to build special routes for us.
  */
 Route::resource('posts', 'PostController');
+
+Route::get('/insert', function (){
+
+	DB::insert('INSERT INTO posts (title, content) VALUES(?, ?)', ['PHP Laravel', 'Laravel is the best thing that has appened to PHP']);
+
+});
+
+Route::get('/read', function () {
+
+	$query_result = DB::select('SELECT * FROM posts ');
+
+	foreach ($query_result as $key => $value) {
+		echo $key . ": " . $value->title . ", " . $value->content;
+	}
+
+});
+
+Route::get('/update', function (){
+
+	DB::update('UPDATE posts SET title = ? WHERE id = ?', ['New title', 1]);
+
+});
+
+Route::get('/delete', function () {
+
+	DB::delete('DELETE FROM posts WHERE id = ?', [1]);
+
+});
