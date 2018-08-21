3ef78b6c22e1ee7a69395d37cb30f4005baf6746
Marco Cilona <marco.cilona@gmail.com>
2018-08-21 12:39:53 +0200

    Classes Lectures.

---
+++ Java/Classes/src/App.java
@@ -0,0 +1,36 @@
+// A class is a blueprint to create objects. In a java project almost everything is an obj.
+/*
+ * A class can contain:
+ * 1. Data
+ * 2. Subroutines (methods)
+ */
+class Person {
+	
+	// Instance variable are data or "state"
+	private String name;
+	private int age;
+	
+	public void setName (String name) {
+		this.name = name;
+	}
+	
+	public String getName () {
+		return this.name;
+	}
+		
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		// Using Person class to create an obj
+		Person firstPerson = new Person();
+		firstPerson.setName("Camillina");
+		
+		System.out.println("Hello " + firstPerson.getName());
+		
+	}
+
+}
