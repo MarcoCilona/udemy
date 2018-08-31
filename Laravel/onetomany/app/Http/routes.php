5b4eb15aa92955c4d6375f95f3d56799a7b3e5b0
Marco Cilona <marco.cilona@gmail.com>
2018-08-31 20:47:28 +0200

    One to many Eloquent CRUD.

--- Laravel/onetomany/app/Http/routes.php
+++ Laravel/onetomany/app/Http/routes.php
@@ -11,6 +11,51 @@
 |
 */
 
+use App\User;
+use App\Post;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function () {
+
+	// Finding the user
+	$user = User::find(3);
+
+	// Creating post instance
+	$post = new Post(['title'=>'Testing one to many', 'body'=>'Testign eloquent one to many']);
+
+	// Saving the new post on the db and relating it with the selected user
+	$user->posts()->save($post);
+
+});
+
+Route::get('/read', function () {
+
+	$user = User::findOrFail(3);
+
+	foreach ($user->posts as $key => $post) {
+	
+		echo $post->title;
+
+	}
+	
+
+});
+
+Route::get('/update', function () {
+
+	$user = User::find(3);
+
+	$user->posts()->where('id', 1)->update(['title'=>'Updated title']);
+
+});
+
+Route::get('/delete', function () {
+
+	$user = User::findOrFail(3);
+
+	$user->posts()->where('id', 1)->delete();
+
 });
