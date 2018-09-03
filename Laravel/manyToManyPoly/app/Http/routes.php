1911c0de10ab51d0ac41d7f65e5decb3eafd9e81
Marco Cilona <marco.cilona@gmail.com>
2018-09-03 11:58:29 +0200

    Many to many poly CRUD.

--- Laravel/manyToManyPoly/app/Http/routes.php
+++ Laravel/manyToManyPoly/app/Http/routes.php
@@ -11,6 +11,27 @@
 |
 */
 
+use App\Post;
+use App\Video;
+use App\Tag;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert', function () {
+
+	$post = Post::create(['name'=>'New post']);
+
+	$tag = Tag::find(1);
+
+	$post->tags()->save($tag);
+
+	
+	$video = Video::create(['name'=>'New video']);
+
+	$tag2 = Tag::find(2);
+
+	$video->tags()->save($tag2);
+
 });
