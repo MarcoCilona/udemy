b0cab6b25df67b794bfca4628fd1033f5f1c20f5
Marco Cilona <marco.cilona@gmail.com>
2018-09-05 20:29:43 +0200

    Polymorphic relationship lectures.

--- Laravel/polyRel/app/Http/routes.php
+++ Laravel/polyRel/app/Http/routes.php
@@ -11,6 +11,40 @@
 |
 */
 
+use App\Staff;
+use App\Photo;
+use App\Product;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function() {
+
+	$staff = Staff::find(1);
+
+	$staff->photos()->create(['path'=>'example.jpg']);
+
+});
+
+Route::get('/read', function() {
+
+	$staff = Staff::findOrFail(1);
+
+	foreach ($staff->photos as $key => $value) {
+		echo $value->path . '<br />';
+	}
+
+});
+
+Route::get('/update', function() {
+
+	$staff = Staff::findOrFail(1);
+
+	$photo = $staff->photos()->where('id', 2)->first();
+
+	$photo->path = "updated.jpg";
+
+	$photo->save();
+
 });
