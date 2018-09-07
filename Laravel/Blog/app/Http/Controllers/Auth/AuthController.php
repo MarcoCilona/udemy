2c309021bdf68867f16628144ca773ab61b4d47e
Marco Cilona <marco.cilona@gmail.com>
2018-09-07 15:16:38 +0200

    Asset with gulp and elixir.

--- Laravel/Blog/app/Http/Controllers/Auth/AuthController.php
+++ Laravel/Blog/app/Http/Controllers/Auth/AuthController.php
@@ -66,6 +66,7 @@ class AuthController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'role_id' => 2,
             'password' => bcrypt($data['password']),
         ]);
     }
